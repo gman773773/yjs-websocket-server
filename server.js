129e86e306ebd222e6fa2488ef3883dd2f5f72b7
gman773773 <grantrothman7@gmail.com>
2025-08-06 15:50:34 -0700

    Fix import to use WebSocketServer

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
-import { setupWSConnection } from './src/y-websocket.js'
-import * as http from 'http'
-import { WebSocketServer } from 'ws'
+import { WebSocketServer } from 'y-websocket/bin/utils.js'
 
 const port = process.env.PORT || 10000
-const server = http.createServer()
-
-const wss = new WebSocketServer({ server })
-
-wss.on('connection', (conn, req) => {
-  setupWSConnection(conn, req)
-})
-
-server.listen(port)
-
 console.log(`✅ y-websocket server running on port ${port}`)
+WebSocketServer({ port })
 
